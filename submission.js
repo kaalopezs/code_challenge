fbc8cc9bc1e8b8de30213b4e935655b321f92382
kaalopezs <70169938+kaalopezs@users.noreply.github.com>
2020-11-12 13:10:28 -0500

    Create submission.js

---
+++ submission.js
@@ -0,0 +1,28 @@
+//Takes an array of integers and returns the sum
+const findSum = function(array) {
+var total = 0;
+for (var i = 0; i < array.length; i++){
+ total += array[i];
+}
+return total;
+};
+
+const findFrequency = function(array) {
+  // your code here - don't forget to return an object!
+};
+
+const isPalindrome = function(str) {
+  // your code here - don't forget to return a boolean!
+};
+
+const largestPair = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+const removeParenth = function(str) {
+  // your code here - don't forget to return a string!
+};
+
+const scoreScrabble = function(str) {
+  // your code here - don't forget to return a number!
+};
