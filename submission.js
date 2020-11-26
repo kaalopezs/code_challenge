ed15dc1901eca99c5852ae8f6f24b178c49f4910
kaalopezs <70169938+kaalopezs@users.noreply.github.com>
2020-11-25 19:46:22 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -65,5 +65,29 @@ const removeParenth = function(str) {
 };
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+let score = 0;
+  for (i = 0; i < str.length; i++){
+    if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' ||str[i] === 'o' ||str[i] === 'u' ||str[i] === 'l' ||str[i] === 'n' ||str[i] === 'r' ||str[i] === 's' ||str[i] === 't'){
+      score++;
+    };
+    if(str[i] === 'd' ||str[i] === 'g'){
+      score += 2;
+    };
+    if(str[i] === 'b' ||str[i] === 'c' ||str[i] === 'm' ||str[i] === 'p'){
+      score +=3;
+    };
+    if(str[i] === 'f' ||str[i] === 'h' ||str[i] === 'v' ||str[i] === 'w' ||str[i] === 'y'){
+      score += 4;
+    };
+    if(str[i] === 'k'){
+      score += 5;
+    };
+    if(str[i] === 'j' ||str[i] === 'x'){
+      score += 8;
+    };
+    if(str[i] === 'z' ||str[i] === 'q'){
+      score +=10;
+    };
+  };
+  return score;
 };
