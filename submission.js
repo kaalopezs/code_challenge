61d521bda20b28d4bfc8a4b03b679ea4aa4cd59d
kaalopezs <70169938+kaalopezs@users.noreply.github.com>
2020-11-25 20:18:23 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -8,45 +8,43 @@ return total;
 };
 
 //Returns the most and least frequent items in the array
- const findFrequency = function(array) {
-  let counter = 0;
-  let mf = 1;
-  let lf = array.length;
-  let mostFrequent;
-  let leastFrequent;
-  for(i = 0; i < array.length; i++){
-    for(j = 0; j < array.length; j++)
-    if(array[i] === array[j])
-      counter++;
-    if(mf < counter){
-      mf = counter;
-      mostFrequent = array[i];
-    }
-    if(lf > counter){
-      lf = counter;
-      leastFrequent = array[i];
-    }
-    counter = 0;
-  }
-  return {
-    'most' : mostFrequent,
-    'least': leastFrequent
+const findFrequency = function(array) {
+ let counter = 0;
+ let mf = 1;
+ let lf = array.length;
+ let mostFrequent;
+ let leastFrequent;
+ for(i = 0; i < array.length; i++){
+  for(j = 0; j < array.length; j++)
+   if(array[i] === array[j])
+    counter++;
+  if(mf < counter){
+   mf = counter;
+   mostFrequent = array[i];
   };
+  if(lf > counter){
+   lf = counter;
+   leastFrequent = array[i];
+  };
+  counter = 0;
+ }
+ return {
+  'most' : mostFrequent,
+  'least': leastFrequent
+ };
 };
 
- const isPalindrome = function(str) {
-  let back = '';
-  str = str.toLowerCase();
-  let word = str.split('');
-  for(i = (word.length - 1); i > -1; i--){
-    back += word[i];
-  }
-  if(back === str){
-    return true;
-  }
-  else{
-    return false;
-  }
+const isPalindrome = function(str) {
+ let back = '';
+ str = str.toLowerCase();
+ let word = str.split('');
+ for(i = (word.length - 1); i > -1; i--){
+  back += word[i];
+ };
+ if(back === str){
+  return true;
+ };
+  return false;
 };
 
 const largestPair = function(array) {
@@ -61,34 +59,34 @@ const largestPair = function(array) {
 };
 
 const removeParenth = function(str) {
-  const noParenth = str.replace(/\(.*\)/, '');
-  return noParenth;
+ const noParenth = str.replace(/\(.*\)/, '');
+ return noParenth;
 };
 
 const scoreScrabble = function(str) {
-let score = 0;
-  for (i = 0; i < str.length; i++){
-    if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' ||str[i] === 'o' ||str[i] === 'u' ||str[i] === 'l' ||str[i] === 'n' ||str[i] === 'r' ||str[i] === 's' ||str[i] === 't'){
-      score++;
-    };
-    if(str[i] === 'd' ||str[i] === 'g'){
-      score += 2;
-    };
-    if(str[i] === 'b' ||str[i] === 'c' ||str[i] === 'm' ||str[i] === 'p'){
-      score +=3;
-    };
-    if(str[i] === 'f' ||str[i] === 'h' ||str[i] === 'v' ||str[i] === 'w' ||str[i] === 'y'){
-      score += 4;
-    };
-    if(str[i] === 'k'){
-      score += 5;
-    };
-    if(str[i] === 'j' ||str[i] === 'x'){
-      score += 8;
-    };
-    if(str[i] === 'z' ||str[i] === 'q'){
-      score +=10;
-    };
+ let score = 0;
+ for (i = 0; i < str.length; i++){
+  if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' ||str[i] === 'o' ||str[i] === 'u' ||str[i] === 'l' ||str[i] === 'n' ||str[i] === 'r' ||str[i] === 's' ||str[i] === 't'){
+   score++;
   };
-  return score;
+  if(str[i] === 'd' ||str[i] === 'g'){
+   score += 2;
+  };
+  if(str[i] === 'b' ||str[i] === 'c' ||str[i] === 'm' ||str[i] === 'p'){
+   score +=3;
+  };
+  if(str[i] === 'f' ||str[i] === 'h' ||str[i] === 'v' ||str[i] === 'w' ||str[i] === 'y'){
+   score += 4;
+  };
+  if(str[i] === 'k'){
+   score += 5;
+  };
+  if(str[i] === 'j' ||str[i] === 'x'){
+   score += 8;
+  };
+  if(str[i] === 'z' ||str[i] === 'q'){
+   score +=10;
+  };
+ };
+ return score;
 };
